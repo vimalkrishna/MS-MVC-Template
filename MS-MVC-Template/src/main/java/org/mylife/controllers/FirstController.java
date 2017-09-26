3694c35433ef1e5954a487a2f3369aa9c0803d80
vimalkrishna <krishnavimal@gmail.com>
2017-09-26 11:09:11 +0200

    Still there will be white label error

---
+++ MS-MVC-Template/src/main/java/org/mylife/controllers/FirstController.java
@@ -0,0 +1,22 @@
+package org.mylife.controllers;
+
+import java.util.Date;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class FirstController {
+	
+	@RequestMapping("/home.html")
+	public ModelAndView firstPage() {
+		// Model Object for JSP
+		Date dateTime = new Date();
+		
+		// return a new Instance of Model and View and pass a name of JSP file
+		//Without view resolver
+		return new ModelAndView("/WEB-INF/jsp/welcome.jsp","dateTime", dateTime);
+	}
+
+}
