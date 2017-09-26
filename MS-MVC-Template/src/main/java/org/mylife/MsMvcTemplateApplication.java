47c18cc512a499df3b90cdf685e3ae0aa2af3e71
vimalkrishna <krishnavimal@gmail.com>
2017-09-26 10:47:39 +0200

    Base commit after creating project that has white lebel error

---
+++ MS-MVC-Template/src/main/java/org/mylife/MsMvcTemplateApplication.java
@@ -0,0 +1,12 @@
+package org.mylife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsMvcTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsMvcTemplateApplication.class, args);
+	}
+}
