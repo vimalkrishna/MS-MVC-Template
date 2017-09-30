1b652d9065c85357412eff98ce38cd535898836c
vimalkrishna <krishnavimal@gmail.com>
2017-09-30 22:12:59 +0200

    Separated Web and REST

--- MS-MVC-Template/src/main/java/org/mylife/config/SecurityConfiguration.java
+++ MS-MVC-Template/src/main/java/org/mylife/config/SecurityConfiguration.java
@@ -1,7 +1,9 @@
 package org.mylife.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -10,36 +12,58 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 @EnableWebSecurity // We don't create Bean class here
 @Profile("production")
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter 
+public class SecurityConfiguration 
 {
 	// Authentication
-	@Override
-	public void configure(AuthenticationManagerBuilder auth) throws Exception
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception
 	{
 		auth.inMemoryAuthentication()
 					.withUser("vimal")
 					.password("krishna")
 					.roles("USER","ADMIN");
 	}
-	
-	
+		
 	// Authorization
-		@Override
-		public void configure(HttpSecurity http) throws Exception
+	// Which order to evaluate the following expressions default order is 100
+	// lowest is evaluated first
+	@Configuration
+	@Order(2)                                                        
+	public static class ApiWebSecurityConfigurationAdapter extends WebSecurityConfigurerAdapter {
+		protected void configure(HttpSecurity http) throws Exception {
+			http
+				.antMatcher("/**")                               
+				.authorizeRequests()
+					.anyRequest().hasRole("ADMIN")
+					.and()
+					.httpBasic()
+					.and()
+					.csrf()
+					.disable(); // For POST needed
+		}
+	}
+
+	// https://docs.spring.io/spring-security/site/docs/current/reference/html/jc.html
+	// Form based
+	@Configuration    
+	@Order(1) 
+	public static class FormLoginWebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
+	
+		public void configureGlobal(HttpSecurity http) throws Exception
 		{
-			// http.antMatcher("/**").csrf().disable().authorizeRequests()
-			http.antMatcher("/**").authorizeRequests()
+			
+			http.antMatcher("/webapp/**").authorizeRequests()
 						.anyRequest().hasRole("USER")
 						
 						.and()
 						
-						.formLogin().loginPage("/login.jsp")
-									.failureUrl("/login.jsp?error=1")
-									.loginProcessingUrl("/login")
+						.formLogin().loginPage("/webapp/login.jsp")
+									.failureUrl("/webapp/login.jsp?error=1")
+									.loginProcessingUrl("/webapp/login")
 									.permitAll()
 									.and()
 									.logout()
 									.logoutSuccessUrl("/webapp/autos/list.html");
 		}
-	
+	}// static class
 }
