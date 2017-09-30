5e522c6b86f59d1d52b5c87786d432a3cc85cc85
vimalkrishna <krishnavimal@gmail.com>
2017-09-30 13:56:57 +0200

    POST for REST

--- MS-MVC-Template/src/main/java/org/mylife/domain/Auto.java
+++ MS-MVC-Template/src/main/java/org/mylife/domain/Auto.java
@@ -21,6 +21,17 @@ public class Auto {
 		return "Auto [name=" + name + ", driver=" + driver + "]";
 	}
 
+	
+	public long getId() {
+		return id;
+	}
+
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+
 	public String getName() {
 		return name;
 	}
--- MS-MVC-Template/src/main/java/org/mylife/rest/AutoRestController.java
+++ MS-MVC-Template/src/main/java/org/mylife/rest/AutoRestController.java
@@ -5,7 +5,11 @@ import java.util.List;
 import org.mylife.controllers.data.AutoRepository;
 import org.mylife.domain.Auto;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /*
@@ -25,8 +29,15 @@ public class AutoRestController {
 		
 	}
 
-	// Now some post method
+	// Now some POST method
 	
+	@RequestMapping(value="/autos", method=RequestMethod.POST)
+	public ResponseEntity<Auto> createAuto(@RequestBody Auto auto ) {
+		data.save(auto);
+		//link to newly created Object
+		return new ResponseEntity<Auto>(auto, HttpStatus.CREATED);
+		
+	}
 	
 	
 	
